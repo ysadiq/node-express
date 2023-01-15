c36ceba86c94c65d3e9ae2c9ee27ba5384f66abb
Yahya Saddiq <yahya.saddiq@gmail.com>
2023-01-15 10:24:17 +0300

    Express Router

---
+++ routes/dishRouter.js
@@ -0,0 +1,31 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const dishRouter = express.Router();
+
+dishRouter.use(bodyParser.json());
+
+dishRouter.route('/')
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Types', 'text/plain');
+        next();
+    })
+    .get((req, res, next) => {
+        res.end('Will send all the dishes to you!');
+    })
+    .post((req, res, next) => {
+        res.end('Will add the dish: ' 
+        + req.body.name 
+        + ' with detail: ' 
+        + req.body.description);
+    })
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation is not supported on /dishes');
+    })
+    .delete((req, res, next) => {
+        res.end('Deleting all the dishes!');
+    });
+
+module.exports = dishRouter;
